aeb38f57e500736410dde1aec2be29b409febfa6
unknown <incontextseo@gmail.com>
2019-07-05 21:25:27 -0500

    Updated the html routes to resolve at homepage not just wildcard

--- server.js
+++ server.js
@@ -39,6 +39,10 @@ app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "app/public/survey.html"));
 });
 
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
+});
+
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "app/public/home.html"));
 });
