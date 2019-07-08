37e5dd46f8b9fd9e89973dbf800647e7833a2e2b
unknown <incontextseo@gmail.com>
2019-07-07 22:14:11 -0500

    Post to friends Array working. Friend match function and modal left to do

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,9 +14,11 @@ module.exports = function(app) {
     app.post("/api/friends", function(req, res) {
         // req.body hosts is equal to the JSON post sent from the user
         // This works because of our body parsing middleware
-        var newFriend = req.data;
+        var newFriend = req.body;
     
-        console.log(newFriend);
+       // var test = res.data;
+       // console.log(req);
+        // console.log(newFriend);
     
         friends.push(newFriend);
     
